d745ffb3679cdbc1b563d878691000f906c6e732
ManuDeveloper <manuepeva@gmail.com>
2020-06-29 12:06:39 -0500

    Reusable Components React

--- server.js
+++
@@ -1,9 +0,0 @@
-function Person(propos){
-    return (<div className="person">
-        <h1>{propos.name}</h1>
-<p>Your Age:{propos.age}</p>
-    </div>);
-};
-
-ReactDOM.render(<Person name="Max" age="28"/>, document.querySelector('#p1'));
-ReactDOM.render(<Person name="Manu" age="30"/>, document.querySelector('#p2'));
