bad79fdd6c9418e062a5b6214970a36588a0749c
ManuDeveloper <manuepeva@gmail.com>
2020-06-29 14:18:14 -0500

    Reusable Components React

--- src/App.js
+++ src/App.js
@@ -10,20 +10,37 @@ const App = () =>{
 
   const [recipes, setRecipes] = useState([]);
 
+  const [search, setSearch] = useState("");
+
+  const [query, setQuery] = useState('chicken');
+
   useEffect(() => {
       getRecipes();
   }, []);
 
   const getRecipes = async () =>{
-        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
         const data = await response.json();
         setRecipes(data.hits);
         console.log('data.hits');
   };
+
+  const updateSearch = e => {
+        setSearch(e.target.value);
+        console.log(search);
+      };
+
+      const getSearch = e => {
+        e.preventDefault();
+        setQuery(search);
+      };
+
   return(
     <div className="App">
       <form className="search-form">
-        <input type="text" className="search-bar"/>
+        <input type="text" className="search-bar" value={search} 
+        onChange={updateSearch}
+        />
           <button type="submit" className="search-button">Search</button>
       </form>
       {recipes.map(recipe => (
